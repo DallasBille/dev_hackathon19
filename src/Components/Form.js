5ee57b52ff20725b389fad9c1996f6b274296341
github username <github email address>
2019-06-19 16:01:53 -0400

    form component created

---
+++ src/Components/Form.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+
+    state = {
+        company: "",
+        website_url: "",
+        username: '',
+        password: ''
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            [event.target.name]: event.target.value
+        },() => console.log(this.state))
+    }
+
+    // console.log(this.state.company);
+    render(){
+        return(
+            <div className="new-form">
+                <form className="">
+                    <h2>Add New Login Details</h2>
+                    <label>Company</label>
+                    <input onChange={this.handleChange} type="text" name="company" value={this.state.company}/>
+                    <br/>
+                    <label>Website URL</label>
+                    <input onChange={this.handleChange} type="text" name="website_url" value={this.state.website_url}/>
+                    <br/>
+                    <label>Username</label>
+                    <input onChange={this.handleChange} type="text" name="username" value={this.state.username}/>
+                    <br/>
+                    <label>Password</label>
+                    <input onChange={this.handleChange} type="text" name="password" value={this.state.password}/>
+                    <br/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Form
---
+++ src/Components/Nav.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+
+class Nav extends Component {
+
+    // state = {
+    //     company: "",
+    //     website_url: "",
+    //     username: '',
+    //     password: ''
+    // }
+
+    // handleChange = (event) => {
+    //     console.log(event.target.value);
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     },() => console.log(this.state))
+    // }
+
+    // console.log(this.state.company);
+    render(){
+        return(
+            <div className="nav">
+                <p>Fuck</p>
+            </div>
+        )
+    }
+}
+
+export default Nav
