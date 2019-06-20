52cd1cd4ee3ad830f1bae2a06cdc967e8fa50079
mnkillebr <killebrew.biz@gmail.com>
2019-06-20 09:08:47 -0400

    Form modal added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ import AccountList from './Containers/AccountList'
 
 class App extends Component {
 
+    openForm = (event) => {
+      document.querySelector('div.form-modal').style.display = 'block'
+    }
+
+    closeForm = (event) => {
+      document.querySelector('div.form-modal').style.display = ''
+    }
+
     render(){
       return (
         <div className="App">
           <Navbar />
+          <button onClick={this.openForm}> Add New </button>
           <div className="profile uk-text-center uk-child-width-1-2@s uk-grid-match uk-grid" data-uk-grid="masonry: true">
-          <Form />
-          <AccountList />
-
+            <Form closeForm={this.closeForm} />
+            <AccountList />
           </div>
 
         </div>
